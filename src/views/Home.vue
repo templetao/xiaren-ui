4ddf4acb655ab50ff052eaf6cd0873c5cb298dc6
taotao <1985504469@qq.com>
2023-04-30 16:53:27 +0800

    Home样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Topnav />
+    <Topnav/>
     <div class="banner">
       <h1>xiaren-UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -15,3 +15,28 @@
 import Topnav from '../components/Topnav.vue'
 
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h / 2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
