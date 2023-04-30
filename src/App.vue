3c02999a671fbd72e86306ccedda0f6639d88f73
taotao <1985504469@qq.com>
2023-04-30 17:23:02 +0800

    完成了路由切换后关闭Aside

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
 </template>
 <script setup lang="ts">
 import {provide, ref} from 'vue'
+import {router} from './router'
 
 const width = document.documentElement.clientWidth
 const menuVisible = ref(width > 500)
 provide('menuVisible', menuVisible) // set
+router.afterEach(() => {
+  if (width <= 500) {
+    menuVisible.value = false
+  }
+})
 </script>
