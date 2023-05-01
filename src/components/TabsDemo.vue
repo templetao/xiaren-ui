cc9c5c3170e346b8d5c0e98096d9888df346e178
taotao <1985504469@qq.com>
2023-05-01 23:01:53 +0800

    创建了Tabs和Tab两个组件，准备写Tabs组件的内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    Tabs
-  </div>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs v-model:aria-selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
+
+<script setup lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import {ref} from 'vue'
+
+const x = ref('导航1')
+</script>
