ffef053c31fc20ce5cb02e40ee61a754588aefbb
taotao <1985504469@qq.com>
2023-05-01 21:22:56 +0800

    实现Dialog基本样式及visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    Dialog
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例 1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x"></Dialog>
 </template>
+
+<script setup lang="ts">
+import Button from '../lib/Button.vue'
+import Dialog from '../lib/Dialog.vue'
+import {ref} from 'vue'
+
+const x = ref(false)
+const toggle = () => {
+  x.value = !x.value
+}
+</script>
