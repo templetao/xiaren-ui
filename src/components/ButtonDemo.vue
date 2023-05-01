82489be5238fbcc56fa9c84d446f712218308a5d
taotao <1985504469@qq.com>
2023-05-01 14:34:22 +0800

    Button添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,17 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-    Button
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
+<script setup lang="ts">
+import Button from '../lib/Button.vue'
+
+const onClick = () => {
+  console.log(111)
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="xiaren-button" :class="{[`theme-${theme}`]: theme}">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  },
+  setup(props, context) {
+    const {...rest} = context.attrs
+    return {rest}
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
