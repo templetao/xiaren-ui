5f8c258d85d4aff2e8d62a2198ecbb1c6ecaba48
taotao <1985504469@qq.com>
2023-05-02 15:51:42 +0800

    改写成 setup格式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -18,58 +18,47 @@
     </div>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
+import {computed, onMounted, ref, useSlots, watchEffect} from 'vue'
 import Tab from './Tab.vue'
-import {computed, ref, onMounted, watchEffect} from 'vue'
 
-export default {
-  props: {
-    selected: String,
-  },
-  setup(props, context) {
-    const selectedItem = ref<HTMLDivElement>(null)
-    const indicator = ref<HTMLDivElement>(null)
-    const container = ref<HTMLDivElement>(null)
+const props = defineProps({
+  selected: String,
+})
+const slots = useSlots()
+const emit = defineEmits(['update:selected'])
+const selectedItem = ref<HTMLDivElement>(null)
+const indicator = ref<HTMLDivElement>(null)
+const container = ref<HTMLDivElement>(null)
 
-    onMounted(() => {
-      watchEffect(
-        () => {
-          const {width} = selectedItem.value.getBoundingClientRect()
-          indicator.value.style.width = width + 'px'
-          const {left: left1} = container.value.getBoundingClientRect()
-          const {left: left2} = selectedItem.value.getBoundingClientRect()
-          const left = left2 - left1
-          indicator.value.style.left = left + 'px'
-        })
-    })
-    const defaults = context.slots.default()
-    defaults.forEach((tag) => {
-      if (tag.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab')
-      }
-    })
+onMounted(() => {
+  watchEffect(() => {
+    const {width} = selectedItem.value.getBoundingClientRect()
+    indicator.value.style.width = width + 'px'
+    const {left: left1} = container.value.getBoundingClientRect()
+    const {left: left2} = selectedItem.value.getBoundingClientRect()
+    const left = left2 - left1
+    indicator.value.style.left = left + 'px'
+  })
+})
 
-    const current = computed(() => {
-      return defaults.find((tag) => tag.props.title === props.selected)
-    })
-
-    const titles = defaults.map((tag) => {
-      return tag.props.title
-    })
-
-    const select = (title: String) => {
-      context.emit('update:selected', title)
-    }
-    return {
-      current,
-      defaults,
-      titles,
-      select,
-      selectedItem,
-      indicator,
-      container,
-    }
-  },
+const defaults = slots.default()
+if (!slots.default) {
+  throw new Error('Tabs 必须至少包含一个 Tab')
+}
+defaults.forEach((tag) => {
+  if (tag.type !== Tab) {
+    throw new Error('Tabs 子标签必须是 Tab')
+  }
+})
+const current = computed(() => {
+  return defaults.find((tag) => tag.props.title === props.selected) || Tab
+})
+const titles = defaults.map((tag) => {
+  return tag.props.title
+})
+const select = (title: String) => {
+  emit('update:selected', title)
 }
 </script>
 
