6c955cb634cd48cf94a2cf59d33d2537b7e3caa6
taotao <1985504469@qq.com>
2023-05-03 15:20:48 +0800

    实现代码展示

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="flag" /&gt;</pre>
+        <pre>{{ Switch1Demo.__sourceCode }}<</pre>
       </div>
     </div>
     <div class="demo">
@@ -22,14 +22,13 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
-import {ref} from 'vue'
 import Switch1Demo from './Switch1Demo.vue'
 import Button from '../lib/Button.vue'
 import Switch2Demo from './Switch2Demo.vue'
@@ -37,9 +36,7 @@ import Switch2Demo from './Switch2Demo.vue'
 export default {
   components: {Switch2Demo, Button, Switch1Demo, Switch},
   setup() {
-    const flag = ref(false)
-    const bool = ref(false)
-    return {flag, bool}
+    return {Switch1Demo, Switch2Demo}
   },
 }
 </script>
@@ -48,22 +45,28 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
+
   > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
+
   &-component {
     padding: 16px;
   }
+
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
+
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    >pre{
+    overflow: auto;
+
+    > pre {
       line-height: 1.2;
       font-family: Consolas, "Courier New", Courier, monospace;
     }
