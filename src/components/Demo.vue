288e2e51a56d99bffcbf01565884701103d6c0f1
taotao <1985504469@qq.com>
2023-05-03 16:30:18 +0800

    优化 Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="demo">
-    <h2>{{component.__sourceCodeTitle}}</h2>
+    <h2>{{ component.__sourceCodeTitle }}</h2>
     <div class="demo-component">
-      <component :is="component" />
+      <component :is="component"/>
     </div>
     <div class="demo-actions">
-      <Button>查看代码</Button>
+      <Button @click="toggleCode">查看代码</Button>
     </div>
-    <div class="demo-code">
-      <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
+    <div class="demo-code" v-if="codeVisible">
+      <pre class="language-html" v-html="html"/>
     </div>
 
   </div>
@@ -16,17 +16,26 @@
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
-import 'prismjs';
+import 'prismjs'
 import 'prismjs/themes/prism-okaidia.css'
+import {computed, ref} from 'vue'
+
 const Prism = (window as any).Prism
 export default {
   components: {Button},
   props: {
     component: Object
   },
-  setup() {
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+    })
+    const codeVisible = ref(false)
+    const toggleCode = () => {
+      codeVisible.value = !codeVisible.value
+    }
     return {
-      Prism
+      Prism, html, codeVisible, toggleCode
     }
   }
 }
@@ -37,22 +46,27 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-  >h2 {
+
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
+
   &-component {
     padding: 16px;
   }
+
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
+
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    >pre {
+
+    > pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
