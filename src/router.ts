51db30a79b352eea02b11a2d7122ddce0223961a
taotao <1985504469@qq.com>
2023-05-04 19:56:01 +0800

    优化显示md文件代码

--- src/router.ts
+++ src/router.ts
@@ -5,9 +5,13 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import Intro from './markdown/intro.md'
+import Install from './markdown/install.md'
+import GetStarted from './markdown/get-started.md'
+import {h} from 'vue'
+import Markdown from './components/Markdown.vue'
+
+const md = (string) => h(Markdown, {content: string, key: string})
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -18,9 +22,9 @@ export const router = createRouter({
       path: '/doc', component: Doc,
       children: [
         {path: '', redirect: '/doc/intro'},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md(Intro)},
+        {path: 'get-started', component: md(GetStarted)},
+        {path: 'install', component: md(Install)},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
