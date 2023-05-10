ad203b5b341c8416de3595dcf5fe68342df42afa
taotao <1985504469@qq.com>
2023-05-10 16:53:36 +0800

    新增 TabsDisabled 组件

---
+++ src/components/TabsDisabled.vue
@@ -0,0 +1,26 @@
+<demo>
+支持 disabled
+</demo>
+<template>
+  <Tabs v-model:selected="TabFlag">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+    <Tab title="导航3" disabled>内容3</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import {ref} from 'vue'
+
+export default {
+  components: {Tabs, Tab,},
+  setup() {
+    const TabFlag = ref('导航1')
+    return {
+      TabFlag
+    }
+  },
+}
+</script>
--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <slot />
+  <div :disabled="disabled">
+    <slot/>
   </div>
 </template>
 <script lang="ts">
 export default {
-  name: "XiarenTab",
-};
+  name: 'XiarenTab',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
 </script>
